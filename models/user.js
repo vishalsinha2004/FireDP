d094dcdda4e345d9232e908715e9bb2f00fa89a8
vishalsinha2004 <vishalsinha6567@gmail.com>
2025-05-24 20:05:10 +0530

    Update MongoDB connection URI in user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+//mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+const uri = "mongodb+srv://user1:Q8X83qYfVrbcZCpL@cluster0.z4v1tyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+//mongodb+srv://user1:<db_password>@cluster0.z4v1tyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+mongoose.connect(uri);
 const userSchema = mongoose.Schema({
     image: String,
     email: String,
