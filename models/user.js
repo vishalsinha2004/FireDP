186ee541554897cf5cdb4f5e9be609c972cdcef0
vishalsinha2004 <vishalsinha6567@gmail.com>
2025-05-24 20:22:51 +0530

    Refactor MongoDB connection logic in user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
-const uri = "mongodb+srv://user1:Q8X83qYfVrbcZCpL@cluster0.z4v1tyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+//const uri = "mongodb+srv://user1:Q8X83qYfVrbcZCpL@cluster0.z4v1tyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//mongoose.connect(uri);
 
-//mongodb+srv://user1:<db_password>@cluster0.z4v1tyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-mongoose.connect(uri);
 const userSchema = mongoose.Schema({
     image: String,
     email: String,
